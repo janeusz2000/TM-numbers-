705887331926bfcedb6e28f1757aae73172abab2
Afatla <agnieszka.fatla.1997@gmail.com>
2018-11-24 18:21:40 +0100

    Add files via upload
    klasyfikator i wywołanie w mainie

--- Main.py
+++ Main.py
@@ -3,18 +3,23 @@
 import GmmObject
 import WaveReader
 import WaveToMfcc
+import Classificator
+
 
 reader = WaveReader.WaveReader("train")
 (signals, rate) = reader.read_all()
 converter = WaveToMfcc.WaveToMfcc(signals, rate)
 mfcc_array = converter.create_mfcc()
 
-table = []
+gmm_table = []
 for each in range(0, 9):
-    table.append(GmmObject.GmmObject(16, mfcc_array[each]))
+    gmm_table.append(GmmObject.GmmObject(16, mfcc_array[each]))
 
-for each in table:
+for each in gmm_table:
     each.train_data()
 
-print("YAY")
+for i in range(0, len(mfcc_array)):
+    classificator = Classificator.Classificator(mfcc_array[i], gmm_table)
+    classification = classificator.classify()
+    print(classification)
 
