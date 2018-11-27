2f7e5e7a440f0e711e56bdd5e5d31d415eeded08
janeusz2000 <45213495+janeusz2000@users.noreply.github.com>
2018-11-27 15:09:35 +0100

    some visual changes

--- Main.py
+++ Main.py
@@ -12,17 +12,17 @@ converter = WaveToMfcc.WaveToMfcc(signals, rate)
 mfcc_array = converter.glue(np.array((1, 2)))
 cross = CrossValidation.CrossValidation(converter.mfcc_array_, 2)
 
-#gmm_table = []
-#for each in range(0, 9):
-    #gmm_table.append(GmmObject.GmmObject(16, mfcc_array[each]))
-
-#for each in gmm_table:
-   #each.train_data()
-
-#for i in range(0, len(mfcc_array)):
-    #classificator = Classificator.Classificator(mfcc_array[i], gmm_table)
-    #classification = classificator.classify()
-    #print(classification)
+# gmm_table = []
+# for each in range(0, 9):
+#    gmm_table.append(GmmObject.GmmObject(16, mfcc_array[each]))
+#
+# for each in gmm_table:
+#    each.train_data()
+#
+# for i in range(0, len(mfcc_array)):
+#    classificator = Classificator.Classificator(mfcc_array[i], gmm_table)
+#    classification = classificator.classify()
+#    print(classification)
 
 
 
