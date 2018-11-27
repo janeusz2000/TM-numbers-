aa8486299469e9ca8dd567e3b97492ffa46e0659
Natquuu <nataliahebda7@gmail.com>
2018-11-27 14:45:46 +0100

    Poprawione WavetoMfcc ale jeszcze trzeba ogarnąć krosswalidacje bo nie wiem co wsadzić do funkcji :/

--- CrossValidation.py
+++ CrossValidation.py
@@ -1,8 +1,15 @@
 import numpy as np
 from sklearn.model_selection import cross_val_score
+from sklearn import datasets, linear_model
+from sklearn.model_selection import train_test_split
+import pandas as pd
 
 
 class CrossValidation(object):
-    def __init__(self, mfcc_input, n_trail):
+    def __init__(self, mfcc_input, n_test):
         self.mfcc_ = mfcc_input
-        self.n_trail_ = n_trail
+        self.n_test_ = n_test
+        df = pd.DataFrame(self.mfcc_.data)
+        y = self.mfcc_.target
+        self.X_train, self.X_test, self.y_train, self.y_test = \
+            train_test_split(df, y, test_size=self.n_test_)
