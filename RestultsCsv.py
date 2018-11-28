e0a113839b353cf3a5d1fa227ec08b25e7e2d6a4
janeusz2000 <45213495+janeusz2000@users.noreply.github.com>
2018-11-28 21:31:28 +0100

    updating results.csv

--- RestultsCsv.py
+++ RestultsCsv.py
@@ -7,13 +7,25 @@ class ResultsCsv(object):
 
     # constructor
 
-    def __init__(self, results):
-        self.results_ = results
-        self.writer = csv.writer("Results.csv")
-
+    def __init__(self, results_matrix):
+        self.results_ = results_matrix
 
     # methods
 
     def write_to_csv(self):
-        print("writing to CSV")
+
+        with open('results.csv', mode='w') as results_file:
+            results_writer = csv.writer(results_file, delimiter=',', quotechar='|', quoting=csv.QUOTE_MINIMAL)
+
+            for site in range(0, 10):
+                site_file = self.results_[site]
+                results_writer.writerow(['Site: '] + [str(site)])
+
+                for each in site_file:
+                    results_writer.writerow([str(each[0])] + [' recognised: '] + [str(each[1])])
+
+        results_file.close()
+        
+
+
 
