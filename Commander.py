a580247a4e1cc5d2db131acf82e6665cbc75f18c
janeusz2000 <45213495+janeusz2000@users.noreply.github.com>
2018-12-05 21:48:48 +0100

    changes in cross_test

--- Commander.py
+++ Commander.py
@@ -49,17 +49,20 @@ class Commander(object):
             trained_gmm = self.train(train_mfcc)
             classificator = Classificator.Classificator([], trained_gmm)
             results_onetest = np.zeros((20, 1))
+            results_likelyhoods = np.zeros((20, 1))
             idx = 0
             names = np.chararray((20, 1), itemsize=12, unicode=True)
             for one_test in test:
                 mfcc_table = self.converter.glue(one_test)
                 for i in range(0, 10):
                     classificator.mfcc_ = mfcc_table[i]
-                    results_onetest[idx, 0] = classificator.classify(i)
+                    results_onetest[idx, 0] = classificator.classify(i)[0]
+                    results_likelyhoods[idx, 0] = classificator.classify(i)[1]
                     names[idx, 0] = self.converter.list_of_speakers[one_test]+"_" + str(i) + '_.wav'
                     idx += 1
             results_onetest = np.concatenate((names, results_onetest), axis=1)
             results.append(results_onetest)
+            results.append(results_likelyhoods)
             rr[0, i_r] = classificator.get_RR()
             i_r += 1
 
